e0988cdf8d735f6c320b50fbc9ee5efad091068f
ZahBenjamin <bz@benzah.com>
2022-02-28 16:52:34 -0500

    Scrapped old logic layout, going for arrays instead

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 var generateBtn = document.querySelector("#generate");
 
+
+var confirmSpecial;
+var Numbers;
+var confirmLower;
+var confirmUpper;
+
+
 function generatePassword() {
   var count = Number(prompt("Please enter the amount of characters you would like in your password."));
-  console.log(count)
+  var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
   if (count < 8 || count > 128) {
     alert("Please put in a valid number between 8-128");
-    console.log("Please put in a valid number between 8-128");
     return writePassword();
   } 
 
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJLKMNOPQRSTUVWXYZ";
-  var number = "0123456789";
-  var special = "~!@#$%^&*()_+=[]{}|<>";
-  var genPassword = '',
-  var selectedCriteria = '',
 
-  if (confirm)
 
-  for (var i = 0; i < count; i++) {
-    var random = Math.floor(Math.random() * lowercase.length) + Math.floor(Math.random() * uppercase.length) + Math.floor(Math.random() * number.length) +specialMath.floor(Math.random() * special.length);
-     genPassword += random;
+  for (var i = 0; i < count ; i++) {
+     genPassword += selectedCriteria[Math.floor(Math.random() * selectedCriteria.length)];
   }
 
-  console.log(genPassword);
 
+  console.log(genPassword);
   return genPassword;
 }
 
